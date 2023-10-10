9e234c3408da0f4e72febe732ec01aa06515bede
Khenya <abrendakhenya@gmail.com>
2023-10-10 14:33:45 -0400

    CRUD Item

---
+++ app/src/test/java/tareas1/JsonComparatorTest.java
@@ -0,0 +1,23 @@
+package tareas1;
+
+// import org.junit.jupiter.api.Test;
+
+// import static org.junit.Assert.*;
+
+public class JsonComparatorTest {
+
+    // @Test
+    // public void testCompareJsons_SameJsons() {
+    //     String expectedJson = "{\"name\": \"John\", \"age\": 30}";
+    //     String actualJson = "{\"name\": \"John\", \"age\": 30}";
+    //     assertTrue(JsonComparator.compareJsons(expectedJson, actualJson));
+    // }
+
+    // @Test
+    // public void testCompareJsons_DifferentJsons() {
+    //     String expectedJson = "{\"name\": \"John\", \"age\": 30}";
+    //     String actualJson = "{\"name\": \"Jane\", \"age\": 25}";
+
+    //     assertFalse(JsonComparator.compareJsons(expectedJson, actualJson));
+    // }
+}
